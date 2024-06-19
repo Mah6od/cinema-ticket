fefda364bf82d90fdeb016f379e8c1a952bba2f3
Mahbod <mahbod.zamanpour@gmail.com>
2024-06-19 13:38:33 +0330

    update files

--- main.py
+++ main.py
@@ -7,16 +7,30 @@ def is_chair_available(reservations, chair_number):
             return False
     return True
 
+# Function to check if all chairs are full
+def are_all_chairs_full(reservations):
+    all_chairs = set(range(1, 101))
+    reserved_chairs = set(chair for reservation in reservations for chair in reservation["chair_numbers"])
+    return all_chairs == reserved_chairs
+
 # Main function to handle reservations
 def make_reservation():
     reservations = load_reservations()
+
+    if are_all_chairs_full(reservations):
+        print("All chairs are full. No more reservations can be made.")
+        return
 
     print("Welcome to the cinema reservation system!")
 
     name = input("Enter your first name: ")
     last_name = input("Enter your last name: ")
     age = int(input("Enter your age: "))
-    num_people = int(input("Enter the number of people: "))
+    num_people = int(input("Enter the number of people (maximum 5): "))
+
+    while num_people < 1 or num_people > 5:
+        print("You can reserve between 1 to 5 chairs.")
+        num_people = int(input("Enter the number of people (maximum 5): "))
 
     chair_numbers = []
     for i in range(num_people):
